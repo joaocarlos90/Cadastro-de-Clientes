8c46df6366073921d8e4b6269a89a0e392e1dffe
Joao Carlos Alves <suporte_carlos@live.com>
2022-06-21 23:29:38 -0300

    building data processing

--- Cadastro_Clientes/src/main/java/org/example/model/Cliente.java
+++ Cadastro_Clientes/src/main/java/org/example/model/Cliente.java
@@ -1,12 +1,15 @@
 package org.example.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Entity
@@ -18,10 +21,16 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(nullable = false, length = 150)
+    @NotEmpty
     private String nome;
+
     @Column(nullable = false, length = 11)
+    @NotNull
+    @CPF
     private String cpf;
+
     @Column(name = "data_cadastro", updatable = false)
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
